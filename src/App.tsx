6b43ed289d84b49ce99127efaebb04fd7e6a5507
rickyfriady <friadi.ricki@gmail.com>
2023-02-03 22:30:51 +0700

    1. Add Navbar component 2. add svg icon 3. add Sansita Font

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
@@ -6,6 +7,7 @@ import Store from "./pages/Store";
 function App() {
   return (
     <div className="container mx-auto mb-4">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/store" element={<Store />}></Route>
