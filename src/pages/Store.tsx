9057caac0b3090d249800f9d9cf72d28f0b01a2e
rickyfriady <friadi.ricki@gmail.com>
2023-02-18 19:55:30 +0700

    change loading to jsx template

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,13 +5,14 @@ import StoreItem from "../components/Storeitem";
 const Store = () => {
   const { isLoading, data }: any = useFetch("https://dummyjson.com/products");
 
-  if (isLoading) return "Loading";
+  if (isLoading) return <div>Loading</div>;
+
   console.log(data.products[1].images[1]);
 
   return (
     <div className="mt-6">
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 sm:gap-2 lg:grid-cols-4 md:gap-4">
-        {data.products.map((items: JSX.IntrinsicAttributes & { id: number; title: string; rating: number; price: number; imgUrl: string }, key: Key | null | undefined) => {
+        {data.products.map((items: JSX.IntrinsicAttributes & { id: number; title: string; rating: number; price: number; imgUrl: string; images: string }, key: Key | null | undefined) => {
           return <StoreItem key={key} {...items} imgUrl={items.images[0]} />;
         })}
       </div>
